e79c604889ce8d6de3f5d83bf25743c4ae453ccc
zena-kebede <151683487+zena-kebede@users.noreply.github.com>
2024-04-20 23:31:50 -0600

    Complete list of project files

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Final
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Final
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
